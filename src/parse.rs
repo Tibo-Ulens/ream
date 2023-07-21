747497ad3cf9c88994c77abb0b8fdad353a7a70d
Tibo Ulens <ulens.tibo@gmail.com>
2023-07-21 04:04:07 +0200

    feat(parser): add parser skeleton code

--- src/parse.rs
+++ src/parse.rs
@@ -1,15 +1,32 @@
+#![allow(dead_code)]
+
+use std::iter::Peekable;
+
 use miette::Error;
+
+use crate::{ast, Lexer, Token};
 
 /// A parser for a single source file
 #[allow(missing_docs)]
 pub struct Parser<'s> {
-	_source: &'s str,
+	source: &'s str,
+	tokens: Peekable<Lexer<'s>>,
 }
 
 impl<'s> Parser<'s> {
 	/// Create a new [`Parser`]
-	pub fn new(source: &'s str) -> Self { Self { _source: source } }
+	pub fn new(source: &'s str, tokens: Peekable<Lexer<'s>>) -> Self { Self { source, tokens } }
+
+	/// Peek at the next [`Token`]
+	///
+	/// Returns [`None`] if no tokens are left
+	fn peek(&mut self) -> Option<&Result<Token<'s>, Error>> { self.tokens.peek() }
+
+	/// Consume and return the next [`Token`]
+	///
+	/// Returns [`None`] if no tokens are left
+	fn next(&mut self) -> Option<Result<Token<'s>, Error>> { self.tokens.next() }
 
 	/// Parse the entire input
-	pub fn parse(&self) -> Result<(), Error> { Ok(()) }
+	pub fn parse(&self) -> Result<ast::Program<'s>, Error> { todo!() }
 }
