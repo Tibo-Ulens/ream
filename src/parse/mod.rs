b0c7f8fc16f27ca0d4e406a6002fb8f2855f8728
Tibo Ulens <ulens.tibo@gmail.com>
2023-07-25 22:27:52 +0200

    refactor(parser): restructure a bit

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -6,6 +6,8 @@ use miette::{Error, SourceSpan};
 
 use crate::{ast, Combine, Lexer, ParseError, Token, TokenType, EOF_TOKEN};
 
+mod annotation;
+
 /// A parser for a single source file
 #[allow(missing_docs)]
 pub struct Parser<'s> {
@@ -158,52 +160,5 @@ impl<'s> Parser<'s> {
 				.into())
 			},
 		}
-	}
-
-	/// Parse an annotation of the form `(:<atom> <identifier> ...)
-	fn parse_annotation(
-		&mut self,
-		initial_span: SourceSpan,
-		annotation_type: &'s str,
-	) -> Result<ast::Annotation<'s>, Error> {
-		match annotation_type {
-			":type" => self.parse_type_annotation(initial_span),
-			":doc" => self.parse_doc_annotation(initial_span),
-			_ => {
-				Err(ParseError::InvalidAnnotation {
-					loc:   initial_span,
-					found: annotation_type.to_string(),
-				}
-				.into())
-			},
-		}
-	}
-
-	/// Parse a type annotation of the form `(:type <identifier> <typespec>)`
-	fn parse_type_annotation(
-		&mut self,
-		_initial_span: SourceSpan,
-	) -> Result<ast::Annotation<'s>, Error> {
-		todo!()
-	}
-
-	/// Parse a doc annotation of the form `(:doc <identifier> <docstring>)`
-	fn parse_doc_annotation(
-		&mut self,
-		initial_span: SourceSpan,
-	) -> Result<ast::Annotation<'s>, Error> {
-		let target = self.expect(TokenType::Identifier(""))?;
-
-		let doc_str_token = self.expect(TokenType::String(""))?;
-		let TokenType::String(doc_str) = doc_str_token.t else { unreachable!() };
-
-		let right_paren = self.expect(TokenType::RightParen)?;
-
-		let span = [&target, &doc_str_token, &right_paren]
-			.iter()
-			.map(|t| t.span)
-			.fold(initial_span, |acc, s| acc.combine(&s));
-
-		Ok(ast::Annotation::DocAnnotation { span, target: target.into(), doc: doc_str })
 	}
 }
